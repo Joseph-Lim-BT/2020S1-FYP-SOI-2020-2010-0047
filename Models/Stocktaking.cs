a913781088765b8125bed95a302c14f6be4ba5e2
Dogephin <64132341+Dogephin@users.noreply.github.com>
2020-08-11 20:25:36 +0800

    Add files via upload

---
+++ Models/Stocktaking.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FYP.Models
+{
+    public class Stocktaking
+    {
+        public int Stocktaking_id { get; set; }
+
+        public int User_id { get; set; }
+
+        public string full_name { get; set; }
+
+        public int total_equipment_quantity { get; set; }
+
+        public int total_accessories_quantity { get; set; }
+
+        public DateTime date_created { get; set; }
+
+        public bool archive { get; set; }
+
+        public int diff_equip { get; set; }
+
+        public int diff_accessory { get; set; }
+
+        [Required(ErrorMessage = "Please select a store to stocktake from")]
+        public string storage_location { get; set; }
+    }
+}
---
+++ Models/Stocktaking_Accessories.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.Models
+{
+    public class Stocktaking_Accessories
+    {
+        public int Stocktaking_Accessories_id { get; set; }
+
+        public int Stocktaking_id { get; set; }
+
+        public int Equipment_accessories_id { get; set; }
+
+        public string Accessories_details { get; set; }
+       
+
+        public string Storage_location { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool Matching { get; set; }
+    }
+}
